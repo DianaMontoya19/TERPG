611c40b7bcc4d19f358453767de1555f8ff721c1
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-03-29 15:55:25 -0500

    Seguimiento Player
    Enemigo sigue al jugador

--- Assets/Script/IAEnemy.cs
+++ Assets/Script/IAEnemy.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class IAEnemy : MonoBehaviour
 {
-    public Transform flag;
-    public Transform positionEnemy; 
+    public Transform[] flag;
+    public Transform positionEnemy;
     private NavMeshAgent agent;
     public GameObject ia;
     private Animator anim;
     public bool destino = false;
     public float speed;
+    private int _i = 0;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-        anim.SetBool("Walk", true);
-        
+
+
 
 
     }
@@ -25,65 +27,107 @@ public class IAEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(flag != null)
+        switch (_i)
         {
-            agent.SetDestination(flag.position);
-            destino = true;
+            case 0:
+                Move(_i = 0);
+                destino = false;
+                break;
+            case 1:
+                Move(_i = 1);
+                if(!destino)
+                {
+                    _i = 0;
+                }
+                else if(destino)
+                {
+                    _i = 2;
+                }
+                break;
+            case 2:
+                Move(_i = 2);
+            break;
         }
 
- 
 
-       
 
+
+
+    }
+
+    public int Move(int _i)
+    {
+        agent.SetDestination(flag[_i].position);
+        anim.SetBool("Walk", true);
+        agent.isStopped = false;
+
+        return _i;
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && _i==0)
         {
-            anim.SetBool("Walk", false);
-            agent.isStopped = true;
-            
-            Destroy(ia);
-            Invoke("ReStart", 1f);
-            Debug.Log("Entro");
-
-
-
+            _i = 2;
+            destino = true;
+            Debug.Log("entro");
+            //anim.SetBool("Walk", false);
+            //agent.isStopped = true;
         }
-        if (destino == true && collision.gameObject.CompareTag("Position"))
-        {
-
-           anim.SetBool("Walk", false);
-            agent.isStopped = true;
-           Debug.Log("llego");
-        }
-
-
     }
-    void ReStart()
+    private void OnTriggerStay(Collider other)
     {
-
-        anim.SetBool("Walk", true);
-        agent.destination = positionEnemy.position;
-        destino = true;
-        agent.isStopped = false;
-        
-        //agent.SetDestination(this.transform.position);
-        Debug.Log("reestart");
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            _i = 1;
+        }
     }
-
-
-    //       if(agent.isStopped)
-    //        {
-    //            agent.isStopped = false;
-    //            agent.destination = positionEnemy.position;
-    //        }
-    //       Debug.Log("el agente llego");
-
-    //       //agent.destination = positionEnemy.position;
-
-    //    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    destino = true;
     //}
 }
+
+            //        Destroy(ia);
+            //        Invoke("ReStart", 1f);
+            //        Debug.Log("Entro");
+
+
+
+            //    }
+            //    if (destino == true && collision.gameObject.CompareTag("Position"))
+            //    {
+
+            //       anim.SetBool("Walk", false);
+            //        agent.isStopped = true;
+            //       Debug.Log("llego");
+            //    }
+
+
+            //}
+            //void ReStart()
+            //{
+
+            //    anim.SetBool("Walk", true);
+            //    agent.destination = positionEnemy.position;
+            //    //destino = true;
+            //    agent.isStopped = false;
+
+            //    //agent.SetDestination(this.transform.position);
+            //    Debug.Log("reestart");
+            //}
+
+
+            //       if(agent.isStopped)
+            //        {
+            //            agent.isStopped = false;
+            //            agent.destination = positionEnemy.position;
+            //        }
+            //       Debug.Log("el agente llego");
+
+            //       //agent.destination = positionEnemy.position;
+
+            //    }
+            //}
+        
