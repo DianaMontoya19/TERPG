5fae6b66ece1c2d465fe3164ac087eefb5263673
Santiago Alzate C <salzate361@misena.edu.co>
2024-04-16 14:30:13 -0500

    Creacion Scripts Menú

---
+++ Assets/Script/UITool/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Más de un AudioMnager en escena.");
+        }
+    }
+
+    public static AudioManager Instance { get; private set; }
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    public void ReproducirSonido(AudioClip audio)
+    {
+
+
+    }
+
+}
